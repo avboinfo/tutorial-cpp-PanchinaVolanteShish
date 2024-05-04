df487f6892944ec352d152c3e6c91d826b28a154
PanchinaVolanteShish <153986272+PanchinaVolanteShish@users.noreply.github.com>
2024-05-04 09:13:52 +0000

    saf

--- olimpiadi/main.cpp
+++ olimpiadi/main.cpp
@@ -1,44 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
 
-int compra(int N, int M, int A, int B) {
+int compra(int N, int M, int A, int B) {  // N è Corse da fare, M corse in 1 carne, A costo di 1 corsa, B costo del carnet
     int x = N/M;
+    int v = A*N; // costo tot big singoli
     int Z;
+
     if (B<A){
-        
-       if (N==M)
-       {
-        return B;
-       }else{
-        if (x==0)
+        if (N%M!=0)
         {
-            Z = x+1;
+            x++;
+            Z=x*B;
+            return Z;
+        }else if (x!=0)
+        {
+            Z=x*B;
             return Z;
         }else{
-            Z=N%M
-            Z= Z*B;
-            return Z;
+            return B;
         }
-        
-        
-       }
-    }else if (N==M)
-    {
-        Z=N*A;
-        return Z;
-    }else if (x>=1)
-    {
-        Z= x*B;
-        x=N%M;
-        x=x*A;
-        Z=Z+x;
-        return Z;
-    }else{
-        Z= N*A;
-        return Z;
     }
+    if (N%M==0){
+        int u = x*B;
+        if (v>u)
+        {
+            Z=x*B;
+            return Z;
+        }else{
+            return v;
+        }
+    }else{
+        
+    }
+    
 }
-
 int main() {
     FILE *fr, *fw;
     int N, M, A, B;
